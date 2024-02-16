d8641465f8be6d36e65acb927e4e8603d0a40aef
Jim Scardelis <jim@jceltd.com>
2024-02-16 13:34:55 -0800

    Clean up a bit. Add alternative symlinks as suggested in issue #1.

--- Casks/ollama-model-updater.rb
+++ Casks/ollama-model-updater.rb
@@ -1,21 +1,26 @@
-cask "ollama-model-updater" do
-    arch arm: "arm64", intel: "x86"
-    
-    version "0.1.7"
-    sha256  arm:    "104a318305e5ac2d1a6c06760d216d8ca9ae43aaab89d719d2b266f3bd0885bf",
-            intel:  "4d38c5c35a81578285e05d13d32de2a25126a4718ec05569299f96be15320d23"
+cask 'ollama-model-updater' do
+  arch arm: 'arm64', intel: 'x86'
+  version '0.1.7'
+  sha256  arm: '04a318305e5ac2d1a6c06760d216d8ca9ae43aaab89d719d2b266f3bd0885bf',
+          intel: '4d38c5c35a81578285e05d13d32de2a25126a4718ec05569299f96be15320d23'
 
-    url "https://github.com/technovangelist/ollamamodelupdater/releases/download/v#{version}/macos-#{arch}-ollamamodelupdate"
+  url "https://github.com/technovangelist/ollamamodelupdater/releases/download/v#{version}/macos-#{arch}-ollamamodelupdate"
 
-    name 'Ollama Model Updater'
-    desc 'Looks at all Ollama models on the system and pulls the mode if updated on ollama.ai'
-    homepage 'https://github.com/technovangelist/ollamamodelupdater'
+  name 'Ollama Model Updater'
+  desc 'Looks at all Ollama models on the system and pulls the mode if updated on ollama.ai'
+  homepage 'https://github.com/technovangelist/ollamamodelupdater'
 
-    livecheck do
-        url :homepage
-        regex(/^v?(\d+(?:\.\d+)+)$/i)
-    end
+  livecheck do
+    url :homepage
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
 
-    binary "macos-#{arch}-ollamamodelupdate", target: 'ollamamodelupdate'
-    binary "macos-#{arch}-ollamamodelupdate", target: 'ollama-model-update'
+  # The original maintainer uses ollamamodelupdate (no r)
+  binary "macos-#{arch}-ollamamodelupdate", target: 'ollamamodelupdate'
+  # For consistency with the repository name, let's link in with the r as well.
+  binary "macos-#{arch}-ollamamodelupdate", target: 'ollamamodelupdater'
+  # I prefer to separate the words with hyphens
+  binary "macos-#{arch}-ollamamodelupdate", target: 'ollama-model-update'
+  # Link in one with both hyphens and the r
+  binary "macos-#{arch}-ollamamodelupdate", target: 'ollama-model-updater'
 end
