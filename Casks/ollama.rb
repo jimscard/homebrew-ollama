43a0b42cdba9036f28ed35dfae07ad62131542bf
Jim Scardelis <jim@jceltd.com>
2024-07-04 18:33:50 -0700

    Fixes for brew style

--- Casks/ollama.rb
+++ Casks/ollama.rb
@@ -1,12 +1,12 @@
-cask "ollama" do
-  version "0.1.31"
-  sha256 "32ecf25804decf13f23d181b438e5d494688bb828e0657c7f077d2bf172efbb6"
+cask 'ollama' do
+  version '0.1.31'
+  sha256 '32ecf25804decf13f23d181b438e5d494688bb828e0657c7f077d2bf172efbb6'
 
   url "https://github.com/ollama/ollama/releases/download/v#{version}/Ollama-darwin.zip",
-      verified: "github.com/ollama/ollama/"
-  name "Ollama"
-  desc "Get up and running with large language models locally"
-  homepage "https://ollama.ai/"
+      verified: 'github.com/ollama/ollama/'
+  name 'Ollama'
+  desc 'Get up and running with large language models locally'
+  homepage 'https://ollama.ai/'
 
   livecheck do
     url :url
@@ -14,14 +14,14 @@ cask "ollama" do
   end
 
   auto_updates true
-  conflicts_with formula: "ollama"
-  depends_on macos: ">= :high_sierra"
+  conflicts_with formula: 'ollama'
+  depends_on macos: '>= :high_sierra'
 
-  app "Ollama.app"
+  app 'Ollama.app'
   binary "#{appdir}/Ollama.app/Contents/Resources/ollama"
 
   zap trash: [
-    "~/.ollama",
-    "~/Library/Application Support/Ollama",
+    '$HOME/.ollama',
+    '$HOME/Library/Application Support/Ollama'
   ]
 end
