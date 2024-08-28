5f20e6bef9ca34a705bb361975c9f5562b475ae2
Jim Scardelis <jim@jceltd.com>
2024-08-28 04:08:09 +0000

    ollama 0.3.8

--- Casks/ollama.rb
+++ Casks/ollama.rb
@@ -1,6 +1,6 @@
 cask "ollama" do
-  version "0.3.6"
-  sha256 "4a2a4ca7bf2378fe75d9975daaccc611797eccc435a2573f41eb8ada6289bd73"
+  version "0.3.8"
+  sha256 "58b81558904328db6e500e09ae4d9aa49c068fb8e9dc616f3e23cde24f897a6d"
 
   url "https://github.com/ollama/ollama/releases/download/v#{version}/Ollama-darwin.zip",
       verified: "github.com/ollama/ollama/"
